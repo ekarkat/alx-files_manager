7e970569690fe3dd89805eb1087b46ff054f72df
ekarkat <ekarkat@gmail.com>
2024-05-15 20:51:17 +0100

    add routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const route = express.Router();
 
@@ -11,5 +12,6 @@ route.post('/users', UsersController.postNew);
 route.get('/connect', AuthController.getConnect);
 route.get('/disconnect', AuthController.getDisconnect);
 route.get('/users/me', UsersController.getMe);
+route.post('/files', FilesController.postUpload);
 
 export default route;
